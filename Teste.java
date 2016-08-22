2b9e77b0d9bb59ecc548fac3775b6cef0cd4bc50
Mauro Patricio <mauro.patricio1@gmail.com>
2016-08-22 15:25:54 +0200

    alteracoes

--- Teste.java
+++ Teste.java
@@ -3,6 +3,7 @@ public class Teste {
 
 	public static void main(String[] args) {
 System.out.println("Primeiro teste usando o System");
+System.out.println("Primeiro teste usando o System");
 	}
 
 }
